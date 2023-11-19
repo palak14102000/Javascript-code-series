23714502932954c97fada2d0969447ae00881f71
palak14102000 <68290163+palak14102000@users.noreply.github.com>
2023-11-19 07:48:55 +0000

    datatypes

---
+++ basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict" // this is used to treat all codes as newer version of javascript
+// alert(3+3) - we are using node js and not browser so this wont work
+let names="palak"
+let num=23;
+let val=true
+let state
+//int - 2 to power 53
+//bigint - bigger numbers
+//string - ""
+//boolean - true/false
+//null - standalone value
+//undefined - value that is undefined
+//symbol - used for unique values
+
+//object
+console.log(typeof names)//string
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
