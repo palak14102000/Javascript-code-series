95baa4e429af21c8c4f874a1e13cd3a313b5866c
palak14102000 <68290163+palak14102000@users.noreply.github.com>
2023-11-20 09:23:38 +0000

    variables

---
+++ basics/01_variable.js
@@ -0,0 +1,9 @@
+const id=12345
+var name="palak"
+let lastname="diwaker"
+var city
+phone=1234566
+
+console.table([id,name,lastname,city,phone])
+// id=35346 -- const cannot be changed
+// refrain from using var keyboard because of the issue of block scope and functional scope
